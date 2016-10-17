8ba656050b0dc34f50ee0428b2e41c0d4d87179b
Peter Miklos <pt.miklos@gmail.com>
2016-10-17 21:39:22 +0100

    transaction_log#deposit test case added, transaction_log updated

--- lib/transaction_log.rb
+++ lib/transaction_log.rb
@@ -1,2 +1,19 @@
 class TransactionLog
+
+  def initialize(transaction_class = Transaction)
+    @transactions = []
+    @transaction_class = transaction_class
+  end
+
+  def deposit(amount)
+    @transactions.push(create_transaction(amount, "deposit"))
+  end
+
+
+  private
+
+  def create_transaction(amount, type)
+    @transaction_class.new(amount, type)
+  end
+
 end
--- spec/transaction_log_spec.rb
+++ spec/transaction_log_spec.rb
@@ -1,1 +1,19 @@
 require 'transaction_log'
+
+describe TransactionLog do
+
+  let(:transaction) {double :transaction}
+  let(:transaction_class) {double(:transaction_class, new: transaction)}
+  subject(:transaction_log) {described_class.new(transaction_class)}
+
+  context "#deposit" do
+    it "create transaction and add it to transactions" do
+      transaction_log.deposit(1000)
+      expect(transaction_log.instance_variable_get(:@transactions)).to include(transaction)
+    end
+  end
+
+  context "#withdraw" do
+
+  end
+end
