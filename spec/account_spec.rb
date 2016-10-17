5988b498f6475a62d810c62c41cb2a9e0e8888eb
Peter Miklos <pt.miklos@gmail.com>
2016-10-17 19:49:29 +0100

    required li and spec files created, first account related test case added

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,16 +1,19 @@
-require 'spec_helper'
+require 'account'
 
 describe Account do
 
-  subject(:account) {described_class.new}
-  let(:transaction_log) {double :transaction_log}
+  let(:transaction_log) {double(:transaction_log, deposit: true)}
   let(:statement) {double :statement}
+  subject(:account) {described_class.new(transaction_log, statement)}
 
   context "#deposit" do
-    
+    it "calls the deposit method on the transaction_log" do
+      expect(transaction_log).to receive(:deposit)
+      account.deposit
+    end
   end
 
-  context "withdrawn" do
+  context "#withdrawn" do
 
   end
 
