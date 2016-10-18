456e1c38d130fca3ad246a80e780a289fe8a97ca
Peter Miklos <pt.miklos@gmail.com>
2016-10-18 10:01:34 +0100

    statement engine updated, relevant test cases passed

--- lib/statement_engine.rb
+++ lib/statement_engine.rb
@@ -3,9 +3,8 @@ class StatementEngine
   def initialize
   end
 
-  def print_statement(transactions)
-    puts add_header
-    # add_statement_content(transactions)
+  def print_statement(transaction_log)
+    add_header + add_statement_content(transaction_log)
   end
 
   private
@@ -14,9 +13,13 @@ class StatementEngine
     "date || credit || debit || balance\n"
   end
 
-  def add_statement_content(transactions)
-    transactions.each do |transaction|
-      # add code here
+  def add_statement_content(transaction_log)
+    transaction_log.transactions.map do |tr|
+      if tr.type == :credit
+        "#{tr.date.strftime("%d/%m/%Y")} || #{sprintf('%.2f', tr.amount)} || || #{sprintf('%.2f', tr.balance)}"
+      else
+        "#{tr.date.strftime("%d/%m/%Y")} || || #{sprintf('%.2f', tr.amount)} || #{sprintf('%.2f', tr.balance)}"
+      end
     end.join("\n")
   end
 
--- spec/statement_engine_spec.rb
+++ spec/statement_engine_spec.rb
@@ -3,13 +3,13 @@ require 'statement_engine'
 describe StatementEngine do
 
   subject(:statement_engine) {described_class.new}
-  let(:transaction1) {double(:transaction1, amount: 1000, type: :deposit, date: Time.new(2016, 10, 13))}
-  let(:transaction2) {double(:transaction2, amount: 500, type: :withdraw, date: Time.new(2016, 10, 17))}
-  let(:transaction_log) {double(:transaction_log, transactionts: [transaction1, transaction2])}
+  let(:transaction1) {double(:transaction1, amount: 1000, type: :credit, date: Time.new(2016, 10, 13), balance: 1200)}
+  let(:transaction2) {double(:transaction2, amount: 500, type: :debit, date: Time.new(2016, 10, 17), balance: 700)}
+  let(:transaction_log) {double(:transaction_log, transactions: [transaction1, transaction2])}
 
   context "#print_statement" do
     it "prints out the statement with header" do
-      result = "date || credit || debit || balance\n13/10/2016 || 1000.00 || || 1000.00\n17/10/2016 || || 500.00 || 500.00"
+      result = "date || credit || debit || balance\n13/10/2016 || 1000.00 || || 1200.00\n17/10/2016 || || 500.00 || 700.00"
       expect(statement_engine.print_statement(transaction_log)).to eq result
     end
   end
